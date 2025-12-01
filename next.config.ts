c37b2e7491501dabe0ead627d2e2634d47fd0892
noblecovn <noblecovn@gmail.com>
2025-12-01 16:55:10 +0700

    25

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.noblecovn.com',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      }
+    ],
+    // Tối ưu hóa hình ảnh
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  }
   /* config options here */
 };
 
