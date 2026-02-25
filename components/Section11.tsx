e5ffc336b976efa0f7af4471db3d30cdcb19d906
noblecovn <noblecovn@gmail.com>
2026-02-25 15:57:54 +0700

    Update certificate image formats in Section11 and Section14 components from JPG to PNG

--- components/Section11.tsx
+++ components/Section11.tsx
@@ -56,7 +56,7 @@ const Section11 = () => {
                         {/* Right - Certificate Image */}
                         <div className="flex justify-center lg:justify-end lg:h-full order-1">
                             <Image
-                                src="/images/section11/ce.jpg"
+                                src="/images/section11/ce.png"
                                 width={3840}
                                 height={2789}
                                 alt="Certification of Completion - Senior Jewelry Consultant Training Program"
